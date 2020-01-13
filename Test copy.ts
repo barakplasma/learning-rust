23f71ed4425b28b5bb2b64b96d37d66e17ddf9c1
Michael Salaverry <michael_salaverry@intuit.com>
2020-01-13 14:30:54 +0200

    removed duplicates

--- Test copy.ts
+++
@@ -1,8 +0,0 @@
-import {expect} from 'chai';
-
-export const Test = {
-    assertSimilar: (actual: any, expected: any, msg = undefined) => expect(actual, msg).to.deep.equal(expected),
-    assertEquals: (actual: any, expected: any, msg = undefined) => expect(actual, msg).to.deep.equal(expected)
-}
-
-Test.assertEquals(1,3);

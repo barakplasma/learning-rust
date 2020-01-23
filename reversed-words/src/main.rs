3be0e1c4b529a8a0d1bb18f3d5500807c740ed8d
Michael Salaverry <michael_salaverry@intuit.com>
2020-01-23 14:37:36 +0200

    feat: channel and thread solution

--- reversed-words/src/main.rs
+++ reversed-words/src/main.rs
@@ -1,19 +1,40 @@
+use std::thread;
+use std::sync::mpsc::channel;
+
 fn main() {
-    println!("{}", reverse_words("hello world"));
+    println!("{}", reverse_words(&"hello world"));
 }
 
-fn reverse_words(str:&str) -> String {
-    let words = str.split_whitespace();
-    let mut sentence = words.collect::<Vec<&str>>();
-    sentence.reverse();
-    sentence.join(" ").to_string()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::reverse_words;
-    #[test]
-    fn returns_expected() {
-      assert_eq!(reverse_words("hello world!"), "world! hello");
+fn reverse_words(str: &'static str) -> String {
+    let words = str.split(" ");
+    let mut output: Vec<String> = vec![];
+    
+    for word in words {
+        // Create a shared channel that can be sent along from many threads
+        // where tx is the sending half (tx for transmission), and rx is the receiving
+        // half (rx for receiving).
+        let (tx, rx) = channel();
+        thread::spawn(move|| {
+            let wr = reverse_one_word(word);
+            tx.send(wr).unwrap();
+        });
+        for received in rx {
+            output.push(received);
+        }
     }
+    
+    output.join(" ")
+}
+
+fn reverse_one_word(str: &str) -> String {
+    str.chars().rev().collect::<String>()
+}
+
+// Rust tests
+#[test]
+fn sample_test() {
+  assert_eq!(reverse_words("The quick brown fox jumps over the lazy dog."), "ehT kciuq nworb xof spmuj revo eht yzal .god");
+  assert_eq!(reverse_words("apple"), "elppa");
+  assert_eq!(reverse_words("a b c d"),"a b c d");
+  assert_eq!(reverse_words("double  spaced  words"), "elbuod  decaps  sdrow");
 }

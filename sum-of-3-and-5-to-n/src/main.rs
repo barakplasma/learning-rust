116f0d88d72b282b10d37b39abbefb9b28f588c4
Michael Salaverry <michael_salaverry@intuit.com>
2020-03-05 15:12:14 +0200

    feat: changed types to better match the problem

--- sum-of-3-and-5-to-n/src/main.rs
+++ sum-of-3-and-5-to-n/src/main.rs
@@ -4,28 +4,28 @@ extern crate quickcheck;
 
 fn main() {
     println!("Hello, world!");
-    let res = find_sum(std::i16::MAX);
+    let res = find_sum(std::u32::MAX);
     println!("{:?}", res);
 }
-// 1229782937674641134
 
-fn sum_of_arithmetic_sequence(n: i64, d: i64) -> i64 {
-    let x = n / d; 
-    return d * x * (x + 1) / 2; 
-}
-fn find_sum(n: i16) -> i64 {
-    sum_of_arithmetic_sequence(n as i64, 3) + sum_of_arithmetic_sequence(n as i64, 5) - sum_of_arithmetic_sequence(n as i64, 15)
+fn sum_of_arithmetic_sequence(n: u32, d: u32) -> u64 {
+    let x: u64 = (n / d) as u64; 
+    return d as u64 * x * (x + 1) / 2; 
 }
 
-#[allow(dead_code)]
-fn sum_of_arithmetic_sequence_ajfaldjfalf(n: i64, d: i64) -> i64 {
-    let x = n / d; 
-    return d * x * (x + 1) / 2; 
+fn find_sum(n: u32) -> u64 {
+    sum_of_arithmetic_sequence(n, 3) + sum_of_arithmetic_sequence(n, 5) - sum_of_arithmetic_sequence(n, 15)
 }
 
 #[allow(dead_code)]
-fn find_sum_solution_11413413(n: i16) -> i64 {
-    sum_of_arithmetic_sequence_ajfaldjfalf(n as i64, 3) + sum_of_arithmetic_sequence_ajfaldjfalf(n as i64, 5) - sum_of_arithmetic_sequence_ajfaldjfalf(n as i64, 15)
+fn sum_of_arithmetic_sequence_ajfaldjfalf(n: u32, d: u32) -> u64 {
+    let x: u64 = (n / d) as u64; 
+    return d as u64 * x * (x + 1) / 2; 
+}
+
+#[allow(dead_code)]
+fn find_sum_solution_11413413(n: u32) -> u64 {
+    sum_of_arithmetic_sequence(n, 3) + sum_of_arithmetic_sequence(n, 5) - sum_of_arithmetic_sequence(n, 15)
 }
 
 #[cfg(test)]
@@ -48,13 +48,14 @@ mod tests {
     fn test_find_sum_1000() {
         assert_eq!(find_sum(1000), 234168);
     }
+
     #[test]
     fn test_find_sum_largest_valid_input() {
-        assert_eq!(find_sum(std::i16::MAX), 250532114);
+        assert_eq!(find_sum(std::u32::MAX), 4304240284008727620);
     }
 
     quickcheck! {
-        fn test_find_sum_quickcheck(n: i16) -> bool {
+        fn test_find_sum_quickcheck(n: u32) -> bool {
             let n2 = n.clone();
             find_sum(n) == find_sum_solution_11413413(n2)
         }

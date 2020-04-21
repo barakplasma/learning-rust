cbda3b6659048fb45babd033bae1ed8269c8b2b5
Michael Salaverry <michael_salaverry@intuit.com>
2020-04-21 10:36:32 +0300

    feat: alternative solution

--- even_numbers/src/main.rs
+++ even_numbers/src/main.rs
@@ -1,18 +1,20 @@
 fn main() {
     even_numbers(&vec![1, 2, 3, 4, 5, 6, 7, 8, 9], 3);
 }
+
+fn is_even<'r>(n: &'r &i32) -> bool {
+    n.checked_rem(2) == Some(0)
+}
 // https://www.codewars.com/kata/5a431c0de1ce0ec33a00000c/train/rust
 fn even_numbers(array: &Vec<i32>, number: usize) -> Vec<i32> {
-    array
-        .iter()
-        .rev()
-        .filter(|n| n.checked_rem(2) == Some(0))
-        .take(number)        
-        .cloned()
-        .collect::<Vec<i32>>()
-        .into_iter()
-        .rev()
-        .collect()
+    let mut last_number_even_numbers_from_array: Vec<i32> = vec![];
+    let mut even_number_iter = array.iter().filter(is_even);
+    for _ in 0..number {
+        last_number_even_numbers_from_array.push(*even_number_iter
+            .next_back().unwrap())
+    }
+    last_number_even_numbers_from_array.reverse();
+    last_number_even_numbers_from_array
 }
 
 #[test]

19c52cd7297082c4b90620d3a581a1f5f5797ce4
Michael Salaverry <michael_salaverry@intuit.com>
2020-05-04 10:22:11 +0300

    feat: works

--- meeting/src/main.rs
+++ meeting/src/main.rs
@@ -2,8 +2,41 @@ fn main() {
     println!("Hello, world!");
 }
 
+use std::cmp::Ordering;
+
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
 fn meeting(s: &str) -> String {
-    unimplemented!()
+    // makes this string uppercase
+    let uppercase_s = s.to_ascii_uppercase();
+    // make the string into a vector of person structs for easier sorting
+    let mut people_structs: Vec<Person> = uppercase_s.split(";").map(|person_string| {
+        let mut parts = person_string.split(":");
+        Person {
+            first_name: parts.next().unwrap().to_string(),
+            last_name: parts.next().unwrap().to_string()
+        }
+    }).collect();
+    // gives it sorted in alphabetical order by last name. When the last names are the same, sort them by first name.
+    people_structs.sort_by(|a, b| {
+        let last_name_ordering = a.last_name.partial_cmp(&b.last_name);
+        match last_name_ordering {
+            Some(Ordering::Equal) => {
+                let first_name_ordering = a.first_name.partial_cmp(&b.first_name);
+                first_name_ordering.unwrap()
+            }
+            _ => last_name_ordering.unwrap(),
+        }
+    });
+    // Last name and first name of a guest come in the result between parentheses separated by a comma.
+    people_structs.iter().fold(String::new(), |mut acc, person| {
+        let pstring = format!("({}, {})", person.last_name, person.first_name);
+        acc.push_str(pstring.as_str());
+        acc
+    })
 }
 
 #[cfg(test)]

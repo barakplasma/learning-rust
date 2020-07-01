dd106b2b0d3fb1fae3e957d70e2e134b6acd2223
Michael Salaverry <michael_salaverry@intuit.com>
2020-07-01 12:36:37 +0300

    better this way

--- first-non-consecutive-number/src/main.py
+++ first-non-consecutive-number/src/main.py
@@ -1,6 +1,6 @@
 def first_non_consecutive(arr):
     previous = None
-    for (i,n) in enumerate(arr):
+    for n in arr:
         if previous and n-1 != previous:
             return n
         previous = n

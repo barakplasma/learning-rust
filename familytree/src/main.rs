1b668c5845e035c99dad4e9781347bfe6d9aaed9
Michael Salaverry <michael_salaverry@intuit.com>
2021-03-22 09:41:38 +0200

    feat: add find siblings in family

--- familytree/src/main.rs
+++ familytree/src/main.rs
@@ -1,17 +1,59 @@
 fn main() {
+    let mut s = Family::new();
+    let mut f = Family::new();
+
     let mimi = Person::root("Mimi");
     let ed = Person::root("Ed");
     let david = Person::new("David", &mimi, &ed);
     let mary = Person::root("Mary");
     let herbert = Person::root("Herbert");
     let sari = Person::new("Sari", &mary, &herbert);
+    let rob = Person::new("Rob", &mary, &herbert);
+
+    f.add_member(&rob);
+    f.add_member(&sari);
+    f.add_member(&herbert);
+    f.add_member(&mary);
+
     let michael = Person::new("Michael", &sari, &david);
     let lia = Person::new("Lia", &sari, &david);
-    lia.display_all_parents();
-    michael.display_all_parents();
+
+    s.add_member(&david);
+    s.add_member(&lia);
+    s.add_member(&michael);
+
+    // michael.display_all_parents();
+    s.siblings(&michael);
+    f.siblings(&sari);
 }
 
-#[derive(Debug)]
+type Members<'people> = Vec<&'people Person<'people>>;
+pub struct Family<'people> {
+    members: Members<'people>,
+}
+
+impl<'people> Family<'people> {
+    fn new() -> Family<'people> {
+        Family { members: vec![] }
+    }
+
+    fn add_member(&mut self, member: &'people Person) {
+        self.members.push(member);
+    }
+
+    fn siblings(&self, individual: &'people Person) {
+        println!("Siblings of {}: ", individual.name);
+        self.members.iter().filter(|m| {
+            match [m.dad, individual.dad] {
+                [Some(dad), Some(dad2)] => dad == dad2,
+                _ => false
+            }
+        }).for_each(|sibling| print!("{}, ", sibling.name));
+        println!("");
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub struct Person<'people> {
     name: String,
     mom: Option<&'people Person<'people>>,
@@ -19,7 +61,7 @@ pub struct Person<'people> {
 }
 
 impl<'people> Person<'people> {
-    fn root(name: &str) -> Person {
+    fn root(name: &str) -> Person<'people> {
         Person {
             name: name.to_string(),
             mom: None,

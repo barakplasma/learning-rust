2a3b5955b0e8c8307652a773d20a7904bcd9c315
Michael Salaverry <michael_salaverry@intuit.com>
2021-03-22 08:31:08 +0200

    feat: multiple children using references and lifetimes

--- familytree/src/main.rs
+++ familytree/src/main.rs
@@ -1,22 +1,24 @@
 fn main() {
     let mimi = Person::root("Mimi");
     let ed = Person::root("Ed");
-    let david = Person::new("David", mimi, ed);
+    let david = Person::new("David", &mimi, &ed);
     let mary = Person::root("Mary");
     let herbert = Person::root("Herbert");
-    let sari = Person::new("Sari", mary, herbert);
-    let michael = Person::new("Michael", sari, david);
+    let sari = Person::new("Sari", &mary, &herbert);
+    let michael = Person::new("Michael", &sari, &david);
+    let lia = Person::new("Lia", &sari, &david);
+    lia.display_all_parents();
     michael.display_all_parents();
 }
 
 #[derive(Debug)]
-pub struct Person {
+pub struct Person<'people> {
     name: String,
-    mom: Option<Box<Person>>,
-    dad: Option<Box<Person>>,
+    mom: Option<&'people Person<'people>>,
+    dad: Option<&'people Person<'people>>,
 }
 
-impl Person {
+impl<'people> Person<'people> {
     fn root(name: &str) -> Person {
         Person {
             name: name.to_string(),
@@ -25,11 +27,11 @@ impl Person {
         }
     }
 
-    fn new(name: &str, mom: Person, dad: Person) -> Person {
+    fn new(name: &str, mom: &'people Person, dad: &'people Person) -> Person<'people> {
         Person {
             name: name.to_string(),
-            mom: Some(Box::new(mom)),
-            dad: Some(Box::new(dad)),
+            mom: Some(mom),
+            dad: Some(dad),
         }
     }
 
@@ -39,11 +41,11 @@ impl Person {
 
     fn display_parent_level(&self, level: usize) {
         self.display_parents(level);
-        match self.dad.as_ref() {
+        match self.dad {
             Some(d) => d.display_parent_level(level + 1),
             None => (),
         }
-        match self.mom.as_ref() {
+        match self.mom {
             Some(m) => m.display_parent_level(level + 1),
             None => (),
         }
